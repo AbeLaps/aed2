028da76a61908b688e3dcea392e9485683e59e8c
Abel Santos <166626908+AbeLaps@users.noreply.github.com>
2025-04-07 10:36:16 -0400

    Update vetor.c
    preencheVetor e preencheVetorOrdenado

--- basicos/vetor.c
+++ basicos/vetor.c
@@ -1,1 +1,25 @@
 /*operacoes do vetor aqui */
+#include <vetor.h>
+
+
+void preencheVetor(tipoVetor *v){
+    srand(time(NULL));
+    unsigned tam = v->tam;
+    int k;
+    for(int i = 0; i < tam; i++){
+        k = rand();
+        v->vet[i] = k;
+    }
+}
+
+void preencheVetorOrdenado(tipoVetor *v){
+    srand(time(NULL));  
+    unsigned tam = v->tam;
+    int k, k1 = 0;
+
+    for(int i = 0; i < tam; i++){
+        k = rand() % 20000 + 1;
+        k1 += k;
+        v->vet[i] = k1;
+    }
+}
