5517079ef8005747b5953588ca66a56e5c3432d0
brunasbrasill <brunabrasil480@gmail.com>
2025-04-07 11:35:14 -0400

    Update vetor.c

--- basicos/vetor.c
+++ basicos/vetor.c
@@ -1,6 +1,7 @@
 /*operacoes do vetor aqui */
 #include <vetor.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 
 void preencheVetor(tipoVetor *v){
     srand(time(NULL));
@@ -22,4 +23,37 @@ void preencheVetorOrdenado(tipoVetor *v){
         k1 += k;
         v->vet[i] = k1;
     }
+}
+
+
+
+void mergeSortR(tipoVetor *v, int aux[], int inicio, int fim) {
+
+  int a,b,k;
+
+  if(inicio < fim) {
+
+    int meio = (inicio+fim)/2;
+    mergeSortR(v, aux, inicio, meio);
+    mergeSortR(v, aux, meio+1, fim);
+  
+    a = inicio;  b = meio+1;   k = inicio;
+    while((a <= meio) && (b<=fim)) {
+      if(v->v[a] < v->v[b]) {    aux[k] = v->v[a];      a++;    }
+      else            {    aux[k] = v->v[b];      b++;    }
+      k++;
+    }
+    while(a <= meio) {    aux[k] = v->v[a];    a++; k++;  }
+    while(b <= fim) {    aux[k] = v->v[b];    b++;    k++;  }
+    for(k = inicio; k <= fim; k++) { v->v[k] = aux[k]; }
+  }
+}
+
+void mergeSort (tipoVetor *v) {
+  int *aux, tam ;
+  tam = v->tam;
+  aux =    (int*) malloc( sizeof(int)*tam);
+
+  mergeSortR(v, aux, 0, tam-1);
+  free(aux);
 }
