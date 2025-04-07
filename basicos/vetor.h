b59b31287502fcecacf4e085d9c0510ed1a59d52
Hooki_Br <ggregoriovitor@gmail.com>
2025-04-07 10:29:25 -0400

    Create vetor.h

---
+++ basicos/vetor.h
@@ -0,0 +1,34 @@
+#ifndef vetor_H
+#define vetor_H
+
+
+typedef struct tipoVetor{
+    unsigned int v[1000000];
+    unsigned int tam;
+}tipoVetor
+
+void criaVetor(tipoVetor * v );
+
+void preencheVetor(tipoVetor v);
+
+void preencheVetorOrdenado(tipoVetor v);
+
+void printaVetor(tipoVetor *v);
+
+
+int buscaSeq(tipoVetor v, int chave);
+
+int buscaBin(tipoVetor v, int chave);
+
+void qSort (tipoVetor * v);
+
+void selSort (tipoVetor * v);
+
+void insertSort (tipoVetor * v);
+
+void bubbleSort (tipoVetor * v);
+
+void mergeSort ( tipoVetor * v);
+
+
+#endif
