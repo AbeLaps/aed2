62154993c7e10ce74e782493d052144b61a71822
brunasbrasill <brunabrasil480@gmail.com>
2025-05-05 11:29:37 -0400

    Create arvore.h

---
+++ T02/basicos/arvore.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct Arv{
+    int info;
+    struct Arv* esq;
+    struct Arv* dir;
+    int fb;
+} Arv ;
+
+Arv* criaArv(int val);
+
+void visita(Arv* arv);
+
+void preFix(Arv* arv);
+
+void posFix(Arv* arv);
+
+void ordem(Arv* arv);
+
+int tamArv(Arv* arv);
+
+void insereArv(Arv* arv, int val);
+
+void criaFilhoRand(Arv* arv, int f);
+
+void popularArvRand(Arv* arv, int tam);
