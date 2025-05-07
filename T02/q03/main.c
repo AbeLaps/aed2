b5c960925f211ee65b5c313e67fbf5ca5ab67373
Abel Santos <166626908+AbeLaps@users.noreply.github.com>
2025-05-07 16:12:23 -0400

    Update main.c
    não consegui rodar no pc do virtu

--- T02/q03/main.c
+++ T02/q03/main.c
@@ -7,20 +7,19 @@
 
 
 int main(){
-    FILE *arquivo1 = fopen("C:/VScode/UFAM-codigos/outputs/tempos_busca_binariaVet.txt", "w");
+    FILE *arquivo1 = fopen("/home/icomp/aed2ss/aed2/T02/q03/tempos_busca_binariaVet.txt", "w");
     if (arquivo1 == NULL) {
         printf("Erro ao abrir arquivo!\n");
         return 1;
     }
-    FILE *arquivo2 = fopen("C:/VScode/UFAM-codigos/outputs/tempos_buscaABP.txt", "w");
+    FILE *arquivo2 = fopen("/home/icomp/aed2ss/aed2/T02/q03/tempos_buscaABP.txt", "w");
     if (arquivo2 == NULL) {
-        printf("Erro ao abrir arquivo!\n");
-        return 1;
+       printf("Erro ao abrir arquivo!\n");
+       return 1;
     }
     
-    tipoVetor v, tempos;
-    criaVetor(&v,3e8);
-    criaVetor(&tempos,30);
+    tipoVetor v;
+    criaVetor(&v,1e3);
 
     preencheVetorOrdenado(&v);
     int n,k;
@@ -32,7 +31,7 @@ int main(){
             k=n;
         }
         else{
-            n = rand() %(int) 1e8; //numero aleatorio dentro do vetor
+            n = rand() %(int) 1e3; //numero aleatorio dentro do vetor
             k=v.vet[n];
         }
         clock_t tempoini = clock();
@@ -40,52 +39,32 @@ int main(){
         clock_t tempof = clock();
         tempo_tot = ((float)(tempof - tempoini) / CLOCKS_PER_SEC);
         fprintf(arquivo1, "%d;%.40f\n", i, tempo_tot);
-        tempos.vet[i] = tempo_tot;
     }
     fclose(arquivo1);
     printf("Tempos salvos em tempos_busca_binariaVet.txt\n");
-
-
-    for(int i = 0;i < 30 ;i++){
-        if(i > 15){ //numero aleatorio
-            n = rand();
-            k=n;
-        }
-        else{
-            n = rand() %(int) 1e8; //numero aleatorio dentro do vetor
-            k=v.vet[n];
-        }
-        clock_t tempoini = clock();
-        buscaBin(v,k);
-        clock_t tempof = clock();
-        tempo_tot = ((float)(tempof - tempoini) / CLOCKS_PER_SEC);
-        fprintf(arquivo1, "%d;%.40f\n", i, tempo_tot);
-        tempos.vet[i] = tempo_tot;
-    }
-    fclose(arquivo1);
-    printf("Tempos salvos em tempos_busca_binariaVet.txt\n");
+    printf("comecando");
 
     Arv* arv = criaArv(v.vet[(v.tam)/2]);
     popularArvVet(arv,&v);
-
+    printf("populei");
     for(int i = 0;i < 30 ;i++){
         if(i > 15){ //numero aleatorio
             n = rand();
             k=n;
         }
         else{
-            n = rand() %(int) 1e8; //numero aleatorio dentro do arvore
+            n = rand() %(int) 1e3; //numero aleatorio dentro do arvore
             k=v.vet[n];
         }
+        printf("fazendo...\n");
         clock_t tempoini = clock();
-        busca(arv,k);
+        buscaABP(arv,k);
         clock_t tempof = clock();
         tempo_tot = ((float)(tempof - tempoini) / CLOCKS_PER_SEC);
-        fprintf(arquivo1, "%d;%.40f\n", i, tempo_tot);
+        fprintf(arquivo2, "%d;%.40f\n", i, tempo_tot);
     }
     fclose(arquivo2);
-    printf("Tempos salvos em tempos_busca_binariaARV.txt\n");
+    printf("Tempos salvos em tempos_busca_binariaABP.txt\n");
 
-    
     return 1;
 }
