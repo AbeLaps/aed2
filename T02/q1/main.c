f5c4d1b38d410aeb71d281c0cf6e22ab64de28a3
Abel Santos <166626908+AbeLaps@users.noreply.github.com>
2025-05-07 10:56:15 -0400

    Update main.c
    Completa!

--- T02/q1/main.c
+++ T02/q1/main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include "ABP.h"
+#include "ABP.c"
 
+
+int main(){
+
+    Arv *arv = criaArv(rand()%1000);
+    popularArvRand(arv, 20);
+    printf("Arvore prefixada:\n");
+    preFix(arv);
+    printf("\nArvore infixada:\n");
+    infix(arv);
+    printf("\nArvore posfixada:\n");
+    posFix(arv);
+
+    return 1;
+}
