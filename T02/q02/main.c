177425424f741e07a11bb8d8781328a1d4e53627
Gabriela Silva Malveira-GABIMALV <gabimalv@motorola.com>
2025-05-12 10:46:16 -0400

    Update main.c q02

--- T02/q02/main.c
+++ T02/q02/main.c
@@ -1,1 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "pacote.h"
 
+
+void simularChegadaDePacotes(ArvPacote* arvore, int totalPacotes, float probabilidadeRetransmissao) {
+    srand(time(NULL));
+
+    for (int i = 0; i < totalPacotes; i++) {
+        int pacote = rand() % 100; 
+        char nome[100];
+        snprintf(nome, sizeof(nome), "Pacote-%d", pacote);
+
+        if (rand() % 100 < probabilidadeRetransmissao * 100) {
+            printf("Retransmissão do pacote: %d\n", pacote);
+        } else {
+            printf("Chegada do pacote: %d\n", pacote);
+            arvore = inserePacote(arvore, pacote, nome);
+        }
+    }
+}
+
+
+void salvarEmArquivo(ArvPacote* arvore, FILE* arquivo) {
+    if (arvore != NULL) {
+        fprintf(arquivo, "ID: %d, Dados: %s\n", arvore->pacote->id, arvore->pacote->dado);
+        salvarEmArquivo(arvore->esq, arquivo);
+        salvarEmArquivo(arvore->dir, arquivo);
+    }
+}
+
+int main() {
+    ArvPacote* arvore = NULL;
+    int totalPacotes = 10;
+    float probabilidadeRetransmissao = 0.2;
+
+    simularChegadaDePacotes(arvore, totalPacotes, probabilidadeRetransmissao);
+
+    printf("Exibindo pacotes na ordem pré-fixada:\n");
+    ordenaArquivo(arvore, stdout);
+
+    FILE* arquivo = fopen("pacotes_recebidos.txt", "w");
+    if (arquivo != NULL) {
+        salvarEmArquivo(arvore, arquivo);
+        fclose(arquivo);
+        printf("Arquivo com pacotes criado com sucesso.\n");
+    } else {
+        printf("Erro ao criar o arquivo.\n");
+    }
+
+    return 0;
+}
