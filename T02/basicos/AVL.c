ae7279f90b3867af012f54d7c4677a34b4bce391
Abel Santos <166626908+AbeLaps@users.noreply.github.com>
2025-05-16 01:55:25 -0400

    Update AVL.c

--- T02/basicos/AVL.c
+++ T02/basicos/AVL.c
@@ -1,256 +1,167 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include "AVL.h"
+#include "avl.h"
 
-Avl* criaAvl(int val, Avl* pai) {
-    Avl* avl;
-    avl = (Avl*)malloc(sizeof(Avl));
-    if (avl == NULL) {
-        printf("morri total");
-        abort();
+// Criar uma nova arvore AVL
+Avl* criarAvl(int valor) {
+    Avl* novo = (Avl*)malloc(sizeof(Avl));
+    if (novo == NULL) {
+        printf("Erro de alocação de memória\n");
+        exit(1);
     }
-    avl->esq = NULL;
-    avl->dir = NULL;
-    avl->info = val;
-    avl->pai = pai;
-    avl->fb = 0; 
-    return avl;
+    novo->info = valor;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    novo->pai = NULL;
+    novo->fb = 0;
+    return novo;
 }
 
-void visitaAvl(Avl* avl) {
-    printf("%d ", avl->info);
+// retorna a altura da arvore
+int alturaAvl(Avl* no) {
+    if (no == NULL) return 0;
+    return 1 + max(alturaAvl(no->esq), alturaAvl(no->dir));
 }
 
-void preFixAvl(Avl* avl) {
-    if (avl != NULL) {
-        visitaAvl(avl);
-        preFixAvl(avl->esq);
-        preFixAvl(avl->dir);
-    }
-}
-
-void posFixAvl(Avl* avl) {
-    if (avl != NULL) {
-        posFixAvl(avl->esq);
-        posFixAvl(avl->dir);
-        visitaAvl(avl);
+// funcao auxiliar da altura
+int max(int a, int b) {
+    if (a > b) {
+    return a;
+    } 
+    else {
+    return b;
     }
 }
 
-void ordemAvl(Avl* avl) {
-    if (avl != NULL) {
-        ordemAvl(avl->esq);
-        visitaAvl(avl);
-        ordemAvl(avl->dir);
-    }
+void atualizarFB(Avl* no) {
+    if (no == NULL) return;
+    no->fb = alturaAvl(no->dir) - alturaAvl(no->esq);
 }
 
-int auxTam(Avl* avl, int tam) {
-    int tamDir = tam, tamEsq = tam;
-    if (avl->dir != NULL) {
-        tamDir += 1;
-        tamDir = auxTam(avl->dir, tamDir);
+// rotacoes:
+Avl* rotacaoEsquerda(Avl* x) {
+    Avl* y = x->dir;
+    x->dir = y->esq;
+    
+    if (y->esq != NULL) {
+        y->esq->pai = x;
     }
-    if (avl->esq != NULL) {
-        tamEsq += 1;
-        tamEsq = auxTam(avl->esq, tamEsq);
-    }
-    if (tamDir > tamEsq) {
-        return tamDir;
+    
+    y->pai = x->pai;
+    
+    if (x->pai == NULL) {
+        y->pai = NULL;
+    } else if (x == x->pai->esq) {
+        x->pai->esq = y;
     } else {
-        return tamEsq;
+        x->pai->dir = y;
     }
+    
+    y->esq = x;
+    x->pai = y;
+    
+    // Update balance factors
+    atualizarFB(x);
+    atualizarFB(y);
+    
+    return y;
 }
 
-int tamAvl(Avl* avl) {
-    if (avl == NULL) {
-        return -1;
-    }
-    int tamEsq = 0, tamDir = 0;
-    Avl* auxDir = avl->dir;
-    Avl* auxEsq = avl->esq;
-    tamDir = auxTam(auxDir, tamDir);
-    tamEsq = auxTam(auxEsq, tamEsq);
-    if (tamDir > tamEsq) {
-        return tamDir;
-    }
-    return tamEsq;
-}
 
-int buscaAvl(Avl* avl, int val) {
-    Avl* atual = avl;
-    while (atual != NULL) {
-        if (val == atual->info) {
-            return 1;
-        } else if (val < atual->info) {
-            atual = atual->esq;
-        } else {
-            atual = atual->dir;
-        }
+Avl* rotacaoDireita(Avl* y) {
+    Avl* x = y->esq;
+    y->esq = x->dir;
+    
+    if (x->dir != NULL) {
+        x->dir->pai = y;
     }
-    return 0;
-}
-
-int altura(Avl* avl) {
-    if (avl == NULL) {return -1;}
-    int alturaEsq = altura(avl->esq);
-    int alturaDir = altura(avl->dir);
-    if (alturaEsq > alturaDir) {
-    return 1 + alturaEsq;
+    
+    x->pai = y->pai;
+    
+    if (y->pai == NULL) {
+        x->pai = NULL;
+    } else if (y == y->pai->dir) {
+        y->pai->dir = x;
     } else {
-    return 1 + alturaDir;
+        y->pai->esq = x;
+    }
+    
+    x->dir = y;
+    y->pai = x;
+    
+    // atualiza os fatores de balanceamento
+    atualizarFB(y);
+    atualizarFB(x);
+    
+    return x;
+}
+
+Avl* balancear(Avl* no) {
+    if (no == NULL) return NULL;
+    
+    atualizarFB(no);
+    
+    // essquerda pesada
+    if (no->fb < -1) {
+        if (no->esq->fb > 0) {
+            // caso LR
+            no->esq = rotacaoEsquerda(no->esq);
+        }
+        // caso LL
+        return rotacaoDireita(no);
+    }
+    // direita pesada
+    else if (no->fb > 1) {
+        if (no->dir->fb < 0) {
+            // caso RL
+            no->dir = rotacaoDireita(no->dir);
+        }
+        // caso RR
+        return rotacaoEsquerda(no);
+    }
+    
+    return no;
+}
+
+// Inserir um valor na arvore. valor igual vai para direita
+Avl* inserirAvl(Avl* raiz, int valor) {
+    if (raiz == NULL) {
+        return criarAvl(valor);
+    }
+    
+    if (valor < raiz->info) {
+        raiz->esq = inserirAvl(raiz->esq, valor);
+        raiz->esq->pai = raiz;
+    } else /*if (valor > raiz->info)*/ {
+        raiz->dir = inserirAvl(raiz->dir, valor);
+        raiz->dir->pai = raiz;
+    } /*else {
+        return raiz;
+    }*/
+    
+    return balancear(raiz);
+}
+
+// free na arvore inteira
+void liberarArvoreAvl(Avl* raiz) {
+    if (raiz != NULL) {
+        liberarArvoreAvl(raiz->esq);
+        liberarArvoreAvl(raiz->dir);
+        free(raiz);
     }
 }
 
-int calcularFB(Avl* avl) {
-    if (avl == NULL) {return 0;}
-    return altura(avl->esq) - altura(avl->dir);
-}
-
-void calcularFBdaArvore(Avl* avl) {
-    if (avl != NULL) {
-        avl->fb = calcularFB(avl);
-    }
-}
-
-void rotEsq(Avl *avl) {
-    if (avl == NULL || avl->dir == NULL) return;
-    
-    Avl* newRoot = avl->dir;
-    Avl* paiOri = avl->pai;
-    
-    avl->dir = newRoot->esq;
-    if (newRoot->esq != NULL) {
-        newRoot->esq->pai = avl;
-    }
-    newRoot->esq = avl;
-    avl->pai = newRoot;
-    
-    // atualiza filho do paiOri's 
-    if (paiOri != NULL) {
-        if (paiOri->esq == avl) {
-            paiOri->esq = newRoot;
+void buscaAvl(Avl* raiz, int key){
+    while (raiz != NULL) {
+        if (raiz->info == key) {
+            return;
+        } else if (key < raiz->info) {
+            raiz = raiz->esq;
         } else {
-            paiOri->dir = newRoot;
+            raiz = raiz->dir;
         }
     }
-    newRoot->pai = paiOri;
-    
-    calcularFBdaArvore(avl);
-    calcularFBdaArvore(newRoot);
+    return;
 }
 
-void rotDir(Avl* avl) {
-    if (avl == NULL || avl->esq == NULL) return;
-    
-    Avl* newRoot = avl->esq;
-    Avl* paiOri = avl->pai;
-    
-    // faz a rotação
-    avl->esq = newRoot->dir;
-    if (newRoot->dir != NULL) {
-        newRoot->dir->pai = avl;
-    }
-    newRoot->dir = avl;
-    avl->pai = newRoot;
-    
-    // atualiza filho do paiOri's 
-    if (paiOri != NULL) {
-        if (paiOri->esq == avl) {
-            paiOri->esq = newRoot;
-        } else {
-            paiOri->dir = newRoot;
-        }
-    }
-    newRoot->pai = paiOri;
-    
-    calcularFBdaArvore(avl);
-    calcularFBdaArvore(newRoot);
-}
-
-void rotDuplaDirEsq(Avl* avl) {
-    if (avl == NULL) return;
-    rotDir(avl->dir);
-    rotEsq(avl);
-}
-
-void rotDuplaEsqDir(Avl* avl) {
-    if (avl == NULL) return;
-    rotEsq(avl->esq);
-    rotDir(avl);
-}
-
-
-void balancear(Avl** avl) {
-    if (*avl == NULL) return;
-
-    calcularFBdaArvore(*avl);
-    
-    if ((*avl)->fb >= 2) {
-        if ((*avl)->dir != NULL && (*avl)->dir->fb < 0) {
-            rotDuplaDirEsq(*avl);
-        } else {
-            rotEsq(*avl);
-        }
-    } else if ((*avl)->fb <= -2) {
-        if ((*avl)->esq != NULL && (*avl)->esq->fb > 0) {
-            rotDuplaEsqDir(*avl);
-        } else {
-            rotDir(*avl);
-        }
-    }
-}
-
-
-void insereValAvl(Avl** avl, int val) {
-    if (*avl == NULL) {
-        *avl = criaAvl(val, NULL);
-        return;
-    }
-
-    Avl* atual = *avl;
-    Avl* pai = NULL;
-
-    while (atual != NULL) {
-        pai = atual;
-        if (val < atual->info) {
-            atual = atual->esq;
-        } else {
-            atual = atual->dir;
-        }
-    }
-
-    Avl* novo_no = criaAvl(val, pai);
-    if (val < pai->info) {
-        pai->esq = novo_no;
-    } else {
-        pai->dir = novo_no;
-    }
-
-    atual = pai;
-    while (atual != NULL) {
-        calcularFBdaArvore(atual);
-        
-        Avl* paiOri = atual->pai;
-        
-        if (atual->fb > 1 || atual->fb < -1) {
-            balancear(&atual);
-            
-            if (paiOri == NULL) {
-                *avl = atual;
-            }
-        }
-        
-        atual = paiOri;
-    }
-}
-
-void liberaAvl(Avl* avl) {
-    if (avl != NULL) {
-        liberaAvl(avl->esq);
-        liberaAvl(avl->dir);
-        free(avl);
-    }
-}
 
