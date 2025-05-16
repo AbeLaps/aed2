f8bd2ba38ef33e362ca9422c9bbfc0b5dced873c
Abel Santos <166626908+AbeLaps@users.noreply.github.com>
2025-05-15 22:37:17 -0400

    Update main.c
    corrigido

--- T02/q04/main.c
+++ T02/q04/main.c
@@ -3,51 +3,45 @@
 #include <time.h>
 #include "AVL.h"
 
+
 int main() {
-    int num_elementos = 1000000; //1e6
-    Avl* arvores[10];
+    int num_elementos = 10000; //1e2
+    Avl* arvore = criaAvl(0, NULL);;
     double tempo_criacao[10];
     int alturas[10];
     double tempo_busca[10];
     double media_busca[10];
 
-
-    srand(time(NULL));
+    srand(0);
     int valores[num_elementos];
     for (int i = 0; i < num_elementos; i++) {
-        valores[i] = rand() %(int) 1e8 ; // Gera valores aleatórios dentro de um intervalo
+        valores[i] = rand(); //%(int) 1e5 ; // Gera valores aleatórios dentro de um intervalo
     }
 
-    printf("--- Criação das Árvores AVL ---\n");
+    printf("--- Criacao das arvores AVL ---\n");
     printf("A");
-     for (int i = 0; i < 10; i++) {
-        printf("cmoecei");
-        arvores[i] = NULL;
+     for (int i = 0; i < 10; i++){
+        printf("comecei");
         clock_t inicio = clock();
-        for (int j = 0; j < num_elementos; j++) {
-            printf("inserindo\n");
-            //ordemAvl(arvores[i]);
-            insereValAvl(&arvores[i], valores[j]);
-            printf("Inserção de %d concluída\n", valores[j]);
-        }
+        printf("inserindo\n");
+        insereValAvl(&arvore, valores[i]);
+        printf("Insercao de %d concluida\n", valores[i]);
         clock_t fim = clock();
         tempo_criacao[i] = (double)(fim - inicio) / CLOCKS_PER_SEC;
-        alturas[i] = altura(arvores[i]);
-        printf("Árvore %d: Tempo de criação = %.4f segundos, Altura = %d\n", i + 1, tempo_criacao[i], alturas[i]);
+        alturas[i] = altura(arvore);
+        printf("arvore %d: Tempo de criação = %.4f segundos, Altura = %d\n", i + 1, tempo_criacao[i], alturas[i]);
      }
 
-    printf("\n--- Busca nas Árvores AVL ---\n");
+    printf("\n--- Busca nas arvores AVL ---\n");
     for (int i = 0; i < 10; i++) {
         tempo_busca[i] = 0.0;
-        for (int j = 0; j < 30; j++) {
-            int valor_busca = valores[rand() % num_elementos]; // Busca por valores que provavelmente estão na árvore
-            clock_t inicio_busca = clock();
-            buscaAvl(arvores[i], valor_busca);
-            clock_t fim_busca = clock();
-            tempo_busca[i] += (double)(fim_busca - inicio_busca) / CLOCKS_PER_SEC;
-        }
+        int valor_busca = valores[rand() % num_elementos]; // Busca por valores que provavelmente estão na árvore
+        clock_t inicio_busca = clock();
+        buscaAvl(arvore, valor_busca);
+        clock_t fim_busca = clock();
+        tempo_busca[i] += (double)(fim_busca - inicio_busca) / CLOCKS_PER_SEC;
         media_busca[i] = tempo_busca[i] / 30;
-        printf("Árvore %d: Tempo total de %d buscas = %.4f segundos, Tempo médio de busca = %.6f segundos\n",
+        printf("arvore %d: Tempo total de %d buscas = %.4f segundos, Tempo medio de busca = %.6f segundos\n",
                i + 1, 30, tempo_busca[i], media_busca[i]);
     }
 
