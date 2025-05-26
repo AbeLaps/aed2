90ed729c5cfd2fdc209b93672cf2eba264507b17
Abel Santos <166626908+AbeLaps@users.noreply.github.com>
2025-05-26 10:16:59 -0400

    Update hash.c

--- T03/basicos/hash.c
+++ T03/basicos/hash.c
@@ -1,1 +1,14 @@
+typedef struct item{
+  int ID;
+  unsigned int idade;
+}item;
 
+
+typedef struct hashItem{
+  struct hashItem * prox;
+  unsigned int ia;
+}hashItem;
+
+typedef struct hashTable{
+ hashItem* vet[10000];
+}hashTable
