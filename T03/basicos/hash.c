52c815171a53872ad62f3adb529bc3882e957f5f
Abel Santos <166626908+AbeLaps@users.noreply.github.com>
2025-06-02 11:56:36 -0400

    Update hash.c

--- T03/basicos/hash.c
+++ T03/basicos/hash.c
@@ -1,8 +1,31 @@
-int hashFunc(item item, const tam){
-    int ind = (item.cpf * 31) % tam;
+
+int hashFunc(long int cpf){
+    int ind = (cpf * 31) % tam;
     return ind;
 }
 
-void inserir(FILE * arq, ABP * arv, hashItem hash[]){
+void criaHashItem(hashItem * itemHash, tipoItem item, unsigned int index){
+    itemhash->index = index;
+    itemHash->prox = NULL;
+    itemHash->cpf = item.cpf;
+    return ;
+}
 
+void popularSistema(FILE * arq, ABP * arv, hashItem hash[]){
+/*inserir no arquivpo
+    pegar o index do registro
+inserir no hash com o indicde do arquivo
+    fazer hash func com o cpf 
+    pegar indice do hash
+    inserir no hash propriamente dito com o cpf,index,verificador de colisao
+inserir na arv abp
+*/
+}
+
+void inserirHash(hashItem hash[],tipoItem item){
+    int ind = hashFunc(item.cpf);
+    criaHashItem
+    if(hash[ind]!= NULL){
+        hash[ind]->prox
+    }
 }
