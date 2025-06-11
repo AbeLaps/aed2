d99f1a43eb50395d1c572ce6054f109b8e6b4e75
brunasbrasill <brunabrasil480@gmail.com>
2025-06-11 11:21:05 -0400

    Update hash.h

--- T03/basicos/hash.h
+++ T03/basicos/hash.h
@@ -3,14 +3,6 @@
 
 #include "ABP.h"
 
-typedef struct tipoItem {
-    long int cpf;         
-    unsigned int idade;
-    int agencia;
-    char nome[65];
-    char email[25];
-} tipoItem;
-
 typedef struct hashItem {
     struct hashItem *prox;
     unsigned int index;
