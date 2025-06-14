74de75aa402f725e4e7272c13fcb2308986723eb
Abel Santos <166626908+AbeLaps@users.noreply.github.com>
2025-06-14 19:32:17 -0400

    Update abp.h

--- T03/basicos/abp.h
+++ T03/basicos/abp.h
@@ -4,21 +4,22 @@
 #include "vetor.h"  // Incluindo vetor.h, pois vamos usar a estrutura de vetor
 
 typedef struct Abp{
-    tipoItem item;
+    long long int info;
     struct Abp* esq;
     struct Abp* dir;
 } Abp;
 
-Abp* criaAbp(tipoItem val);
+
+Abp* criaAbp(long long int);
 void visita(Abp* Abp);
 void preFix(Abp* Abp);
 void posFix(Abp* Abp);
 void inFix(Abp* Abp);
-int tamAbp(Abp* Abp);
-void insereValAbp(Abp* Abp, tipoItem val);
-void popularAbpRand(Abp* Abp, int tam);
+//int tamAbp(Abp* Abp);
+void insereValAbpCpf(Abp* Arv, long long int val);
+//void popularAbpRand(Abp* Abp, int tam);
 void liberarArvoreABP(Abp* raiz);
-void coletaDadosAbp(Abp* Abp, tipoVetor *vetor, int *index);
-void calcularEstatisticas(Abp* Abp, tipoVetor *vetor);
+//void coletaDadosAbp(Abp* Abp, tipoVetor *vetor, int *index);
+//void calcularEstatisticas(Abp* Abp, tipoVetor *vetor);
 
 #endif
