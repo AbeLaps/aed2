614d9f3b34e5f4e77cb0bd21e7271cc96a795a70
Abel Santos <166626908+AbeLaps@users.noreply.github.com>
2025-06-15 23:57:37 -0400

    Update abp.h

--- T03/basicos/abp.h
+++ T03/basicos/abp.h
@@ -1,7 +1,23 @@
 #ifndef ABP_H
 #define ABP_H
 
-#include "vetor.h"  // Incluindo vetor.h, pois vamos usar a estrutura de vetor
+//#include "vetor.h"  
+
+/*
+Integrantes do grupo:
+Abel Andrade Prazeres dos Santos
+Gabriela Silva Malveira
+Bruna de Souza Brasil
+Gabriel Gregório dos Santos Vitor
+*/
+
+typedef struct tipoItem {
+    long long int cpf;
+    unsigned int idade;
+    int agencia;
+    char nome[65];
+    char email[25];
+} tipoItem;
 
 typedef struct Abp{
     tipoItem item;
@@ -9,20 +25,26 @@ typedef struct Abp{
     struct Abp* dir;
 } Abp;
 
-
 Abp* criaAbp(tipoItem item);
-void visita(Abp* Abp);
-void preFix(Abp* Abp);
-void posFix(Abp* Abp);
-void inFix(Abp* Abp);
-//int tamAbp(Abp* Abp);
+
 void insereValAbpIdade(Abp* Arv, tipoItem);
+
 void insereValAbpCpf(Abp* Arv, tipoItem);
+
 int buscaAbp(Abp* arv, tipoItem chave);
-void buscaInterAbp(Abp *raiz, tipoItem idadeBusca, int intervaloMin, int intervaloMax, int flag);
+
+void buscaInterAbp(Abp* raiz, int idadeBase, int flag);
+
+/*
+void visita(Abp* Abp);
+//void preFix(Abp* Abp);
+//void posFix(Abp* Abp);
+//void inFix(Abp* Abp);
+//int tamAbp(Abp* Abp);
 //void popularAbpRand(Abp* Abp, int tam);
-void liberarArvoreABP(Abp* raiz);
+//void liberarArvoreABP(Abp* raiz);
 //void coletaDadosAbp(Abp* Abp, tipoVetor *vetor, int *index);
 //void calcularEstatisticas(Abp* Abp, tipoVetor *vetor);
+*/
 
 #endif
