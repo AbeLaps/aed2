8a20f974376d2fa896f54513187dc760affb2d74
Abel Santos <166626908+AbeLaps@users.noreply.github.com>
2025-06-15 23:58:01 -0400

    Update hash.h

--- T03/basicos/hash.h
+++ T03/basicos/hash.h
@@ -4,11 +4,20 @@
 #include "abp.h"
 #include <stdio.h>
 
+/*
+Integrantes do grupo:
+Abel Andrade Prazeres dos Santos
+Gabriela Silva Malveira
+Bruna de Souza Brasil
+Gabriel Gregório dos Santos Vitor
+*/
+
 typedef struct hashItem {
     struct hashItem *prox;
     unsigned int index;
     long int cpf;
 } hashItem;
+
 
 int hashFunc(long int cpf);
 void criaHashItem(hashItem *itemHash, tipoItem item, unsigned int indexArq);
@@ -17,6 +26,6 @@ void inserirHash(hashItem hash[], tipoItem item,int indArq);
 hashItem buscaHash(hashItem hash[], long int cpf);
 //int EscreverNoArquivo(hashItem *item, FILE *fp, int *pos);
 void popularSistema(FILE *arq, Abp *arvCpf,Abp *arvIdade, hashItem hash[]);
-void buscaArq(FILE *arquivo, long long int idadeBusca, int intervaloMin, int intervaloMax, int flag);
+void buscaArq(FILE *arquivo, int idadeBusca, int flag);
 void buscaSeqArq(FILE *arquivo, long long int cpfBusca);
 #endif
