89432547f43e5fd292562d100bb6ed8466271e9d
Abel Santos <166626908+AbeLaps@users.noreply.github.com>
2025-06-15 23:57:49 -0400

    Update hash.c

--- T03/basicos/hash.c
+++ T03/basicos/hash.c
@@ -1,17 +1,24 @@
 #include "hash.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "abp.h"
 
-#define tam 100
+
+/*
+Integrantes do grupo:
+Abel Andrade Prazeres dos Santos
+Gabriela Silva Malveira
+Bruna de Souza Brasil
+Gabriel Gregório dos Santos Vitor
+*/
+
+#define TAM_HASH 10000
 
 int posicaoInsert = 0;
 hashItem hashNULO = {NULL, 0, -1};
 
 int hashFunc(long int cpf) {
-    return (cpf * 10007) % tam;
+    return (cpf * 10007) % TAM_HASH;
 }
-
 
 void criaHashItem(hashItem *itemHash, tipoItem item, unsigned int indexArq) {
     itemHash->index = indexArq;
@@ -19,41 +26,50 @@ void criaHashItem(hashItem *itemHash, tipoItem item, unsigned int indexArq) {
     itemHash->cpf = item.cpf;
 }
 
-void inicializaHash(hashItem hash[]){
-    for(int i = 0; i < 10000; i++){
-    hash[i] = hashNULO;
+void inicializaHash(hashItem hash[]) {
+    for (int i = 0; i < TAM_HASH; i++) {
+        hash[i].cpf = -1;
+        hash[i].prox = NULL;
+        hash[i].index = 0;
     }
 }
 
 void inserirHash(hashItem hash[], tipoItem item,int indArq) {
-    int ind = hashFunc(item.cpf);  // calcula o índice usando o CPF
-    hashItem novoItem;
-    criaHashItem(&novoItem, item, indArq);
+    int ind = hashFunc(item.cpf);
 
-    // encadeamento caso já tenha um objeto posicionado
-    if (hash[ind].cpf != -1 ) {
-        novoItem.prox = &hash[ind];
-        hash[ind] = novoItem;
+    if (hash[ind].cpf == -1) {
+        // Posição vazia, insere direto
+        hash[ind].cpf = item.cpf;
+        hash[ind].index = indArq;
+        hash[ind].prox = NULL;
     } else {
-        novoItem.prox = &hashNULO;
-        hash[ind] = novoItem;  // caso contrário, simplesmente coloca na posição
+        // Colisão: cria novo nó dinamicamente e insere no início da lista
+        hashItem* novo = malloc(sizeof(hashItem));
+        if (!novo) {
+            printf("Erro de alocação!\n");
+            return;
+        }
+        novo->cpf = item.cpf;
+        novo->index = indArq;
+        novo->prox = hash[ind].prox;
+        hash[ind].prox = novo;
     }
 }
 
 hashItem buscaHash(hashItem hash[], long int cpf) {
     int ind = hashFunc(cpf);
-    hashItem aux = hash[ind];
+    hashItem* aux = &hash[ind];
 
-    // percorre a lista encadeada até encontrar o CPF ou chegar ao final
-    while (aux.cpf != -1) {
-        printf("andando");
-        printf("%ld\n",aux.prox);
-        printf("%d", &aux);
-        if (aux.cpf == cpf) {
-            return aux;
+    // Percorre lista encadeada
+    while (aux != NULL && aux->cpf != -1) {
+        if (aux->cpf == cpf) {
+            return *aux;  // retorna o item encontrado (cópia)
         }
-        aux = *(aux.prox);
+        aux = aux->prox;
     }
+
+    // Se não encontrar, retorna algum valor indicando "não achou"
+    // Assumindo que hashNULO é uma variável global do tipo hashItem com cpf = -1
     return hashNULO;
 }
 
@@ -69,35 +85,32 @@ void popularSistema(FILE *arq, Abp *arvCpf, Abp *arvIdade, hashItem hash[]){
     
 }
 
-void buscaArq(FILE *arquivo, long long int idadeBusca, int intervaloMin, int intervaloMax, int flag) {
+void buscaArq(FILE *arquivo, int idadeBusca, int flag) {
     tipoItem item;
     int encontrou = 0;
 
-    // Volta ao início do arquivo
+    if (flag < 1 || flag > 4) {
+        printf("Flag invalida! Use 1 (<), 2 (>), 3 (<=), 4 (>=)\n");
+        return;
+    }
+
     fseek(arquivo, 0, SEEK_SET);
 
     while (fread(&item, sizeof(tipoItem), 1, arquivo) == 1) {
-        //printf("%ld\n",item.cpf);
-        // Verifica se a idade do item está no intervalo exclusivo (intervaloMin, intervaloMax)
-        if (item.idade > intervaloMin && item.idade < intervaloMax) {
-            int condicao = 0;
+        int condicao = 0;
 
-            switch (flag) {
-                case 1: condicao = (item.idade < idadeBusca); break;
-                case 2: condicao = (item.idade > idadeBusca); break;
-                case 3: condicao = (item.idade <= idadeBusca); break;
-                case 4: condicao = (item.idade >= idadeBusca); break;
-                default:
-                    printf("Flag invalida! Use 1 (<), 2 (>), 3 (<=), 4 (>=)\n");
-                    return;
-            }
-
-            if (condicao) {
-                encontrou = 1;
-                //printf("CPF: %lld | Idade: %u | Agencia: %d | Nome: %s | Email: %s\n",
-                       //item.cpf, item.idade, item.agencia, item.nome, item.email);
-            }
+        switch (flag) {
+            case 1: condicao = (item.idade < idadeBusca); break;
+            case 2: condicao = (item.idade > idadeBusca); break;
+            case 3: condicao = (item.idade <= idadeBusca); break;
+            case 4: condicao = (item.idade >= idadeBusca); break;
         }
+        //comentado para gerar os tempos de forma real
+        // if (condicao) {
+        //     encontrou = 1;
+        //     printf("CPF: %lld | Idade: %u | Agencia: %d | Nome: %s | Email: %s\n",
+        //            item.cpf, item.idade, item.agencia, item.nome, item.email);
+        // }
     }
 
     if (!encontrou) {
